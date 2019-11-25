8837e83dd45dc12d90703db57a64196db0dce54e
dungpb <badungphan99@gmail.com>
2019-11-25 23:37:54 +0700

    fix docker-compose file

--- manage.py
+++ manage.py
@@ -4,7 +4,10 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Shell
 from app import create_app, db
 from dotenv import load_dotenv
-from app.models import user, quiz, forum, course
+from app.models.user import *
+from app.models.quiz import *
+from app.models.forum import *
+from app.models.course import *
 
 load_dotenv(dotenv_path='./env/flask_setting.env')
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -21,6 +24,12 @@ def recreate_db():
 
     db.drop_all()
     db.create_all()
+    db.session.commit()
+
+@manager.command
+def migrate_type_account():
+    type_account = TypeAccount('teacher')
+    db.session.add(type_account)
     db.session.commit()
 
 if __name__ == '__main__':
