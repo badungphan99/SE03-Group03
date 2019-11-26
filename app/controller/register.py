db62f7ff1f161f0e0f3bbb63f8993f21bfcb7111
dungpb <badungphan99@gmail.com>
2019-11-26 17:23:41 +0700

    update register new user

--- app/controller/register.py
+++ app/controller/register.py
@@ -1,54 +1,53 @@
 from app.models import *
 from app import db
 
+
 def check_user_name(username):
     user = db.session.query(User).filter(User.username.like(username)).first()
-    if(user):
+    if (user):
         return 1
     else:
         return 0
 
+
 def check_input_register(username, password, retypepassword, fullname, email):
-    
-    #check username
+    # check username
     if username == None:
         return "Nhap username"
     for c in username:
         if ord(c) < 97 or ord(c) > 119:
             return "username khong duoc co ky tu dac biet"
 
-    #check pass
+    # check pass
     if len(password) < 8 or password == None:
         return "Mat khau phai tren 8 ky tu"
     if password != retypepassword:
         return "Mat khau khac nhau, nhap lai"
-    
-    #check full name
+
+    # check full name
     if fullname == None:
         return "Nhap ten"
     for c in fullname:
         if ord(c) < 97 or ord(c) > 119:
             return "Ten khong duoc co ky tu dac biet"
 
-    #check email
+    # check email
     cntA = 0
     cntD = 0
     for c in email:
         if c == '@':
-            cntA = cntA+1
+            cntA = cntA + 1
         if c == '.':
-            cntD = cntD+1
+            cntD = cntD + 1
     if cntA != 1 and cntD != 1:
         return "Nhap sai email"
-    
+
     return True
 
-def insert_new_user(type_user, username, passwd, fullname, email, birthday = None, highest_degree = None, university = None, major = None):
+
+def insert_new_user(type_user, username, passwd, fullname, email, birthday, highest_degree, university, major):
     type_account = TypeAccount(type_user)
     user = User(username, passwd, fullname, email)
     # type_account.users.append(user)
     db.session.add(user)
     db.session.commit()
-
-if __name__ == "__main__":
-    print(check_input_register("asfkjasfw", "asfasfasf", "asfasfasf", "wiofoasdaswfowfw", "ashaksfha@k.jsfhwf"))
--- app/routes/register.py
+++ app/routes/register.py
@@ -10,15 +10,25 @@ def render_register():
 def register():
     _username = request.form.get('username')
     _passwd = request.form.get('password')
+    _retype_passwd = request.form.get('retypepassword')
+    _full_name = "dungpb"
     _email = request.form.get('email')
     _birthday = request.form.get('birthday')
     _type_account = 'student'
-    _full_name = "dungpb"
-    print(_username, _passwd, "dungpb", _email,)
-    if(check_user_name(_username)):
-        flash("username already exist")
-        return redirect('/register.html')
+    _highest_degree = ''
+    _university = ''
+    _major = ''
+
+    status = check_input_register(_username, _passwd, _retype_passwd, _full_name, _email)
+
+    if(status == True):
+        if (check_user_name(_username)):
+            flash("username already exist")
+            return redirect('/register.html')
+        else:
+            insert_new_user(_type_account, _username, _passwd, _full_name, _email, _birthday, _highest_degree, _university, _major)
+            flash("success")
+            return render_template('register.html')
     else:
-        insert_new_user(_type_account, _username, _passwd, _full_name, _email)
-        flash("success")
+        flash(status)
         return redirect('/register.html')
