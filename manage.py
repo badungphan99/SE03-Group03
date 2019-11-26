0f5693b9f35dbc3dd04de40b8b6f466f4811d414
dungpb <badungphan99@gmail.com>
2019-11-26 23:28:10 +0700

    init test

--- manage.py
+++ manage.py
@@ -6,20 +6,33 @@ from dotenv import load_dotenv
 from app.models import *
 from app.routes import *
 
+# load_dotenv(dotenv_path='./env/flask_setting.env')
+# app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-@manager.command()
-def create_db():
+@manager.command
+def recreate_db():
+    """
+    Recreates a local database. Not use this on production
+    """
+    db.drop_all()
     db.create_all()
     db.session.commit()
 
-@manager.command()
-def runapp():
+@manager.command
+def migrate_type_account():
+    type_account = TypeAccount('student')
+    db.session.add(type_account)
+    db.session.commit()
+@manager.command
+def runcode():
     app.run(host='0.0.0.0', port=80)
 
 if __name__ == '__main__':
