cf9401a9e6e3ba7b03836a30d76fa320b7f97a02
dungpb <badungphan99@gmail.com>
2019-11-26 23:10:18 +0700

    init test

--- manage.py
+++ manage.py
@@ -6,9 +6,21 @@ from dotenv import load_dotenv
 from app.models import *
 from app.routes import *
 
+manager = Manager(app)
+migrate = Migrate(app, db)
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-if __name__ == '__main__':
+@manager.command()
+def create_db():
+    db.create_all()
+    db.session.commit()
+
+@manager.command()
+def runapp():
     app.run(host='0.0.0.0', port=80)
+
+if __name__ == '__main__':
+    manager.run()
