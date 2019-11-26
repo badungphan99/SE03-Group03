db05b3f80b0e0406cf1043917a063fd6f1850410
hoangi19 <viethoanghy99@gmail.com>
2019-11-26 17:09:38 +0700

    add check_input_register

--- app/controller/register.py
+++ app/controller/register.py
@@ -8,9 +8,47 @@ def check_user_name(username):
     else:
         return 0
 
+def check_input_register(username, password, retypepassword, fullname, email):
+    
+    #check username
+    if username == None:
+        return "Nhap username"
+    for c in username:
+        if ord(c) < 97 or ord(c) > 119:
+            return "username khong duoc co ky tu dac biet"
+
+    #check pass
+    if len(password) < 8 or password == None:
+        return "Mat khau phai tren 8 ky tu"
+    if password != retypepassword:
+        return "Mat khau khac nhau, nhap lai"
+    
+    #check full name
+    if fullname == None:
+        return "Nhap ten"
+    for c in fullname:
+        if ord(c) < 97 or ord(c) > 119:
+            return "Ten khong duoc co ky tu dac biet"
+
+    #check email
+    cntA = 0
+    cntD = 0
+    for c in email:
+        if c == '@':
+            cntA = cntA+1
+        if c == '.':
+            cntD = cntD+1
+    if cntA != 1 and cntD != 1:
+        return "Nhap sai email"
+    
+    return True
+
 def insert_new_user(type_user, username, passwd, fullname, email, birthday = None, highest_degree = None, university = None, major = None):
     type_account = TypeAccount(type_user)
     user = User(username, passwd, fullname, email)
     # type_account.users.append(user)
     db.session.add(user)
     db.session.commit()
+
+if __name__ == "__main__":
+    print(check_input_register("asfkjasfw", "asfasfasf", "asfasfasf", "wiofoasdaswfowfw", "ashaksfha@k.jsfhwf"))
