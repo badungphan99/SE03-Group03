8fa3eafbcb8c32fb28f30993fc39831ec832bdc2
dungpb <badungphan99@gmail.com>
2019-11-26 23:40:01 +0700

    update code

--- app/routes/course.py
+++ app/routes/course.py
@@ -2,5 +2,6 @@ from app import app
 from flask import render_template
 
 @app.route('/block_coursera.html', endpoint='render_course')
+@login_required
 def render_course():
     return render_template('block_coursera.html')
