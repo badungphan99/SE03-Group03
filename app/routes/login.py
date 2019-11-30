26019a0093804fff8f37315b05c5da6d546c54a9
hoangi19 <viethoanghy99@gmail.com>
2019-11-30 14:32:19 +0000

    update login

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,26 +1,22 @@
 from app import *
 from app.controller import *
-from flask import render_template, request, flash, redirect, make_response
-import json
+from flask import render_template, request, flash, redirect, url_for
 
 @app.route('/login.html', endpoint='render_login')
 def render_login():
     return render_template('login.html')
 
-@app.route('/login', endpoint='process_login', methods=['POST'])
+@app.route('/login', endpoint='process_login', methods=['POST', 'GET'])
 def process_login():
     _username = request.form.get('username')
     _passwd = request.form.get('password')
+    
     status = check_login(_username, _passwd)
-    print(status)
     if status == True:
-        res = make_response(json.dumps("Dang nhap thanh cong"))
-        res.status_code = 200
-        return res
-    else:
-        res = make_response(json.dumps("Nhap sai username hoac password"))
-        res.status_code = 200
-        return res
+        return redirect('block_course.html')
+        session['username'] = _username
+    flash(status)
+    return redirect('/login.html')
 
 @app.route('/logout')
 @login_required
