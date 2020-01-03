2d93e97b05c63b9f6966efa867064b317c0327b7
hoangi19 <viethoanghy99@gmail.com>
2020-01-03 18:10:14 +0000

    fix trang chu

--- app/routes/home.py
+++ app/routes/home.py
@@ -6,7 +6,7 @@ from app.controller import *
 
 @app.route('/', endpoint='render_root')
 def render_root():
-    return render_template('block_home.html')
+    return render_home()
 
 @app.route('/block_home.html', endpoint='render_home')
 def render_home():
