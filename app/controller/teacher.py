1aed5521e65b255f174fa37cb45022e30f87d1a1
Kim Cuc Nguyen <53978241+kimcucnguyen@users.noreply.github.com>
2020-01-10 14:06:14 +0700

    Insert teacher course

--- app/controller/teacher.py
+++ app/controller/teacher.py
@@ -1,10 +1,19 @@
 from app.models import *
 from app import db
 
-def insert_teacher_course(_name_course, _des, _duration, _topic):
+def insert_teacher_course(user_id,_name_course, _des, _duration, _topic):
     course = Course(_name_course, _des, _duration)
     topic = db.session.query(Topic).filter(Topic.id == _topic).first()
+    user = db.session.query(User).filter(User.id == user_id).first()
+
     if topic:
         topic.coursers.append(course)
+
+    if user:
+        teachCourse = TeacherCourse()
+        teachCourse.course = course
+        user.courses.append(teachCourse)
+
     db.session.add(course)
     db.session.commit()
+
