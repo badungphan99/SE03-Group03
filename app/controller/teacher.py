d0e0ce780eca5b1b474e49793aec3b3415bfc677
Kim Cuc Nguyen <53978241+kimcucnguyen@users.noreply.github.com>
2020-01-10 14:56:43 +0700

    Create course

--- app/controller/teacher.py
+++ app/controller/teacher.py
@@ -5,20 +5,24 @@ def insert_teacher_course(user_id,_name_course, _des, _duration, _topic):
     print(user_id)
     print(_topic)
     print(_name_course)
-    course = Course(_name_course, _des, _duration)
+    course1 = Course(_name_course, _des, _duration)
     topic = db.session.query(Topic).filter(Topic.id == _topic).first()
     print(topic.topic_name)
     user = db.session.query(User).filter(User.id == user_id).first()
     # # print(topic)
     # # print()
-    if topic:
-        topic.coursers.append(course)
 
-    if user:
-        teachCourse = TeacherCourse()
-        teachCourse.course = course
-        user.courses.append(teachCourse)
-
-    db.session.add(course)
+    teachCourse = TeacherCourse()
+    teachCourse.course = course1
+    user.courses.append(teachCourse)
+    db.session.add(course1)
     db.session.commit()
+
+    topic.coursers.append(course1)
+    db.session.commit()
+
+
+
+
+
 
