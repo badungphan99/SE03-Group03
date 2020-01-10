e22f078fb7785a6910bda70b49ff23f76ac235d1
Kim Cuc Nguyen <53978241+kimcucnguyen@users.noreply.github.com>
2020-01-10 16:02:16 +0700

    Upload file

--- app/controller/upload.py
+++ app/controller/upload.py
@@ -2,20 +2,20 @@ from app.models import *
 from app import *
 from sqlalchemy import and_
 
-def insert_file_location(location):
-    fileLocation = FileLocation(location)
-    db.session.add(fileLocation)
-    db.session.commit()
+# def insert_file_location(location):
+#     fileLocation = FileLocation(location)
+#     db.session.add(fileLocation)
+#     db.session.commit()
+#
+# def insert_file_type(_type):
+#     file_type = FileType(_type)
+#     db.session.add(file_type)
+#     db.session.commit()
 
-def insert_file_type(_type):
-    file_type = FileType(_type)
-    db.session.add(file_type)
-    db.session.commit()
+def insert_file_to_db(user_id, fileName, pathInServer, section_id):
+    user = db.session.query(User).filter(User.id == user_id).first()
+    fileUpload = FileUpload(fileName, pathInServer, section_id)
+    user.file_uploads.append(fileUpload)
 
-def insert_file_to_db(fileName, pathInServer, fileLocation, file_type):
-    fileUpload = FileUpload(fileName, pathInServer)
-    # fileUpload.user_id = user_id
-    file_location = FileLocation(fileLocation)
-    file_location.file_uploads.append(fileUpload)
     db.session.add(fileUpload)
     db.session.commit()
--- app/models/file.py
+++ app/models/file.py
@@ -1,34 +1,37 @@
 from app import db
 
-__all__ = ["FileUpload", "FileLocation", "FileType"]
+# __all__ = ["FileUpload", "FileLocation", "FileType"]
+__all__ = ["FileUpload"]
+
 class FileUpload(db.Model):
     __tablename__ = 'file_upload'
     id = db.Column(db.Integer, primary_key=True)
     file_name = db.Column(db.String(250), nullable=False)
     path_in_server = db.Column(db.Text(), nullable=False)
-    file_type_id = db.Column(db.Integer,  db.ForeignKey('file_type.id'))
+    # file_type_id = db.Column(db.Integer,  db.ForeignKey('file_type.id'))
     user_id = db.Column(db.Integer,  db.ForeignKey('user.id'))
-    location_id = db.Column(db.Integer,  db.ForeignKey('file_location.id'))
+    # location_id = db.Column(db.Integer,  db.ForeignKey('file_location.id'))
     file_id = db.Column(db.Integer)
 
-    def __init__(self, fileName, pathInServer):
+    def __init__(self, fileName, pathInServer, file_id):
         self.file_name = fileName
         self.path_in_server = pathInServer
+        self.file_id = file_id
 
-class FileLocation(db.Model):
-    __tablename__ = 'file_location'
-    id = db.Column(db.Integer, primary_key=True)
-    location = db.Column(db.String(100), nullable=False)
-    file_uploads = db.relationship('FileUpload', backref='file_location', lazy='dynamic')
+# class FileLocation(db.Model):
+#     __tablename__ = 'file_location'
+#     id = db.Column(db.Integer, primary_key=True)
+#     location = db.Column(db.String(100), nullable=False)
+#     file_uploads = db.relationship('FileUpload', backref='file_location', lazy='dynamic')
+#
+#     def __init__(self, location):
+#         self.location = location
 
-    def __init__(self, location):
-        self.location = location
-
-class FileType(db.Model):
-    __tablename__ = 'file_type'
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(100), nullable=False)
-    file_uploads = db.relationship('FileUpload', backref='file_type', lazy='dynamic')
-
-    def __init__(self, type):
-        self.type = type
+# class FileType(db.Model):
+#     __tablename__ = 'file_type'
+#     id = db.Column(db.Integer, primary_key=True)
+#     type = db.Column(db.String(100), nullable=False)
+#     file_uploads = db.relationship('FileUpload', backref='file_type', lazy='dynamic')
+#
+#     def __init__(self, type):
+#         self.type = type
